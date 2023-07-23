94aeaadbbbcfcdd31c0fb0c1896b1b7fc3f69d69
MUHAMMADQASIM6936 <119961355+MUHAMMADQASIM6936@users.noreply.github.com>
2023-07-23 18:05:40 +0500

    Update App.js

--- App.js
+++ App.js
@@ -18,32 +18,17 @@ function App() {
 
   return (
     // <>
-       <Router >
+       
      
     
         <Navbar title="TextUtils" aboutText="About us" />
       
         <div className='container my-3' >
         <TextForm heading="Enter the text to analyze " />
-        <Routes> 
+       
         
         
-        <Route exact path="/About" element={<About/>}>
-     
-        </Route>
-         <Route path="/" ></Route>
-         {/* <TextForm heading="Enter the text to analyze " />]
-        </Route>  */}
-      
-
-        </Routes>
-
-        </div>
        
-
-       
-        </Router>
- 
    
 
   )
